9afe10d101d375d1693c0b1e9836844f598eff6b
atash <atash@LAPTOP-R2LDVLRR>
2024-03-14 21:11:54 -0500

    Add AstraDB integration to main.py and update .gitignore files
    This commit introduces a new file, main.py, which integrates AstraDB for storing and querying document data. Also, .gitignore files are updated to ignore the .env file which stores confidential tokens, and certain IntelliJ IDEA configuration and GitHub Copilot files that are not necessary to track within the Git repository.

---
+++ main.py
@@ -0,0 +1,58 @@
+from astrapy.db import AstraDB
+from dotenv import load_dotenv
+import os
+
+
+# Load .env file
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print("Hello World")
+
+    # Now we can access the variables
+    TOKEN = os.getenv("TOKEN")
+    API_ENDPOINT = os.getenv("AOI_ENDPOINT")
+
+    # Initialize the client
+    db = AstraDB(
+        token=TOKEN,
+        api_endpoint=API_ENDPOINT)
+
+    print(f"Connected to Astra DB: {db.get_collections()}")
+
+    # Create a collection. The default similarity metric is "cosine".
+    collection = db.create_collection("vector_test", dimension=5, metric="cosine")
+    print(collection)
+
+    # Insert documents into the collection
+    documents = [
+        {
+            "_id": "1",
+            "text": "ChatGPT integrated sneakers that talk to you",
+            "$vector": [0.1, 0.15, 0.3, 0.12, 0.05],
+        },
+        {
+            "_id": "2",
+            "text": "An AI quilt to help you sleep forever",
+            "$vector": [0.45, 0.09, 0.01, 0.2, 0.11],
+        },
+        {
+            "_id": "3",
+            "text": "A deep learning display that controls your mood",
+            "$vector": [0.1, 0.05, 0.08, 0.3, 0.6],
+        },
+    ]
+    res = collection.insert_many(documents)
+    print(res)
+
+    # Perform a similarity search
+    query = [0.15, 0.1, 0.1, 0.35, 0.55]
+    results = collection.vector_find(query, limit=2, fields={"text", "$vector"})
+
+    for document in results:
+        print(document)
+
+    # Delete the collection
+    res = db.delete_collection(collection_name="vector_test")
+    print(res)
